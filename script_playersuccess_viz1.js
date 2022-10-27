a5c1b3c9d8c644971689695f01d12e5db814f1ab
Kaleb Knecht <kknecht@clemson.edu>
2022-10-27 14:14:50 -0400

    Fix unrelated comments

---
+++ script_playersuccess_viz1.js
@@ -0,0 +1,31 @@
+var svg_2 =  d3.select("#teamsuccess_viz2")
+  .style("width", width*0.7)
+  .style("height", height)
+  .attr("x", 850)
+
+console.log(width)
+
+let border_2 = svg_2.append("g")
+                .append("rect")
+                .attr("stroke-width", 2)
+                .attr("stroke", "black")
+                .attr("fill", "none")
+                .attr("y", 25)
+                .attr("width", width*0.7)
+                .attr("height", height-25)
+
+var title = svg_2.append("text") 
+                .text('Team Success') 
+                .attr("text-anchor", "middle") 
+                .style("font-size", '24px') 
+                .attr("dy", 20)
+                .attr("dx", border_2.attr("width") / 2);
+
+var temp_holder_2 = svg_2.append("text")
+                    .text('<Visualization 2>')
+                    .attr("text-anchor", "middle")
+                    .style("font-size", "12px")
+                    .attr("dx", border_2.attr("width")/2)
+                    .attr("dy", border_2.attr("height")/2)
+
+console.log(border_2.attr("width"))
