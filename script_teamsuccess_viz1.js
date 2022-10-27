efb9d70f7569a59e351930dd03a87e1d9fec9f47
Kaleb Knecht <kknecht@clemson.edu>
2022-10-27 12:54:37 -0400

    First viz skeleton created

--- script_teamsuccess_viz1.js
+++ script_teamsuccess_viz1.js
@@ -1,16 +1,34 @@
-var width = 500
+var width = 800
 var height= 500
 
-var svg =  d3.select("#nba")
+var svg =  d3.select("#teamsuccess_viz1")
   .style("width", width)
   .style("height", height)
 
+var title = svg.append("text") 
+              .text('Team Success') 
+              .attr("text-anchor", "middle") 
+              .style("font-size", '24px') 
+              .attr("dy", 20)
+              .attr("dx", width / 2);
+
 let border = svg.append("g")
                 .append("rect")
-                .attr("stroke", "black")
                 .attr("stroke-width", 2)
-                .attr("width", 50)
-                .attr("height", 50)
+                .attr("stroke", "black")
+                .attr("fill", "none")
+                .attr("y", 25)
+                .attr("width", width)
+                .attr("height", height-25)
+
+var temp_holder = svg.append("text")
+                    .text('<Visualization 1>')
+                    .attr("text-anchor", "middle")
+                    .style("font-size", "12px")
+                    .attr("dx", border.attr("width")/2)
+                    .attr("dy", border.attr("height")/2)
+
+console.log(border.attr("width"))
 
   /*
 let bound = svg.append("g")
