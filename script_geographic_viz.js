d38efce847c4f98b36028570f1e7eb90b4e58bbd
Kaleb Knecht <kknecht@clemson.edu>
2022-10-27 15:08:29 -0400

    Geographic Skeleton Complete

---
+++ script_geographic_viz.js
@@ -0,0 +1,31 @@
+var geographic_svg =  d3.select("#geographic_viz")
+  .style("width", width)
+  .style("height", height)
+  .attr("x", 850)
+
+console.log(width)
+
+let geographic_border = geographic_svg.append("g")
+                .append("rect")
+                .attr("stroke-width", 2)
+                .attr("stroke", "black")
+                .attr("fill", "none")
+                .attr("y", 25)
+                .attr("width", width)
+                .attr("height", height-25)
+
+var title = geographic_svg.append("text") 
+                .text('Geographic Chart') 
+                .attr("text-anchor", "middle") 
+                .style("font-size", '24px') 
+                .attr("dy", 20)
+                .attr("dx", playersuccess_border.attr("width") / 2);
+
+var temp_holder_2 = geographic_svg.append("text")
+                    .text('<Geographic Chart>')
+                    .attr("text-anchor", "middle")
+                    .style("font-size", "12px")
+                    .attr("dx", geographic_border.attr("width")/2)
+                    .attr("dy", geographic_border.attr("height")/2)
+
+console.log(geographic_border.attr("width"))
---
+++ script_spiderchart.js
@@ -0,0 +1,24 @@
+var spiderchart_svg =  d3.select("#spiderchart")
+  .style("width", width*0.7)
+  .style("height", height)
+  .attr("x", 850)
+
+console.log(width)
+
+let spiderchart_border = spiderchart_svg.append("g")
+                .append("rect")
+                .attr("stroke-width", 2)
+                .attr("stroke", "black")
+                .attr("fill", "none")
+                .attr("y", 25)
+                .attr("width", width*0.7)
+                .attr("height", height-25)
+
+var temp_holder_2 = spiderchart_svg.append("text")
+                    .text('<Spider/Radar Chart>')
+                    .attr("text-anchor", "middle")
+                    .style("font-size", "12px")
+                    .attr("dx", spiderchart_border.attr("width")/2)
+                    .attr("dy", spiderchart_border.attr("height")/2)
+
+console.log(spiderchart_border.attr("width"))
