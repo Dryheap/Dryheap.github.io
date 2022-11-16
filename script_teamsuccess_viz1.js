e92165b555a56016642476daabeac98c1fc296df
Kaleb Knecht <kknecht@clemson.edu>
2022-11-16 14:35:36 -0500

    alterations to groups / subgroups

--- script_teamsuccess_viz1.js
+++ script_teamsuccess_viz1.js
@@ -34,6 +34,62 @@ d3.csv("nba_adv_data.csv").then(
                     .attr("width", dimensions.width)
                     .attr("height", dimensions.height-25)
 
+
+    // This is where the fun begins
+
+    // get True Shooting Percent
+    var subgroups = dataset.columns.slice(1)
+    console.log(subgroups)
+
+    // get team info
+    var teams = d3.map(dataset, function(d){return(d.Tm)}).keys()
+
+    console.log(teams)
+
+    // X-axis
+    var x = d3.scaleBand()
+      .domain(teams)
+      .range([0, dimensions.width])
+      .padding([0.2])
+
+    svg.append("g")
+      .attr("transform", "translate(0," + dimensions.height + ")")
+      .call(d3.axisBottom(x).tickSize(0));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, 40])
+      .range([dimensions.height, 0]);
+
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // x-axis Subgroup Positions
+    var xSubgroup = d3.scaleBand()
+      .domain(subgroups)
+      .range([0, x.bandwidth()])
+      .padding([0.05])
+
+    // choose color
+    var color = "#38c9b4"
+
+    // draw the graph
+    svg.append("g")
+        .selectAll("g")
+        // Enter in data = loop group per group
+        .data(dataset)
+        .enter()
+        .append("g")
+          .attr("transform", function(d) { return "translate(" + x(d.group) + ",0)"; })
+        .selectAll("rect")
+        .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+        .enter().append("rect")
+          .attr("x", function(d) { return xSubgroup(d.key); })
+          .attr("y", function(d) { return y(d.value); })
+          .attr("width", xSubgroup.bandwidth())
+          .attr("height", function(d) { return height - y(d.value); })
+          .attr("fill", function(d) { return color; });
+
     var temp_holder = svg.append("text")
                         .text('<Visualization 1>')
                         .attr("text-anchor", "middle")
