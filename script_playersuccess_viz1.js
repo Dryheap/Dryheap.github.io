7a4a3817b31f95fbdec44b6c0bee9dd32e7f19bc
Kaleb Knecht <kknecht@clemson.edu>
2022-11-17 09:36:04 -0500

    OLD teamsuccess_viz2 -> NEW playersuccess_viz1

--- script_playersuccess_viz1.js
+++ script_playersuccess_viz1.js
@@ -3,48 +3,87 @@ d3.csv("nba_adv_data.csv").then(
   function(dataset) {
 
     var dimensions = {
-      width: 1000,
-      height: 700,
+      width: 700,
+      height: 600,
       margin:{
           top: 60,
           bottom: 70,
           right: 10,
           left: 80
       }
-  }
-
-    var playersuccess_svg =  d3.select("#playersuccess_viz1")
-      .style("width", dimensions.width * .5)
+    }
+    var svg_2 =  d3.select("#playersuccess_viz1")
+      .style("width", dimensions.width)
       .style("height", dimensions.height)
-      .attr("y", 850)
+      .attr("x", 850)
+      //.attr("transform", "translate(300,155)")
 
     console.log(dimensions.width)
 
-    let playersuccess_border = playersuccess_svg.append("g")
+   let border_2 = svg_2.append("g")
                     .append("rect")
                     .attr("stroke-width", 2)
                     .attr("stroke", "black")
-                    .attr("fill", "none")
+                    .attr("fill", "None")
                     .attr("y", 25)
-                    .attr("width", dimensions.width * .7)
-                    .attr("height", dimensions.height-25)
-                    .attr("y", "100")
+                    .attr("width", dimensions.width)
+                    .attr("height", dimensions.height)
 
-    var title = playersuccess_svg.append("text") 
+    var title = svg_2.append("text") 
                     .text('Player Success') 
                     .attr("text-anchor", "middle") 
                     .style("font-size", '24px') 
                     .attr("dy", 20)
-                    .attr("dx", playersuccess_border.attr("width") / 2);
+                    .attr("dx", border_2.attr("width") / 2);
 
-    var temp_holder = playersuccess_svg.append("text")
-                        .text('<Visualization 3>')
-                        .attr("text-anchor", "middle")
-                        .style("font-size", "12px")
-                        .attr("dx", playersuccess_border.attr("width")/2)
-                        .attr("dy", playersuccess_border.attr("height")/2)
+    var players = d3.map(dataset, function(d){return d["Player"]})
 
-    console.log(playersuccess_border.attr("width"))
+    var xAccessor = d => d["WS"]
+    var yAccessor = d => d["VORP"]
+
+    var xScale = d3.scaleLinear()
+                   .domain([-.6, 2.7]) 
+                   .range([dimensions.margin.left, dimensions.width - dimensions.margin.right])
+    
+    var yScale = d3.scaleLinear()
+                   .domain([-.4, 1.6])
+                   .range([dimensions.height-dimensions.margin.bottom, dimensions.margin.top])
+  
+    var dotColor = d3.scaleLinear().domain(yScale.domain()).range(["#8790b1", "#ff6818"])
+    var dotSize = d3.scaleLinear().domain(xScale.domain()).range([2, 6])
+
+    var dots = svg_2.append("g")
+                    .selectAll("circle")
+                    .data(dataset, function(d){return d})
+                    .enter()
+                    .append("circle")
+                    .attr("cx", d => xScale(xAccessor(d)))
+                    .attr("cy", d => yScale(yAccessor(d)))
+                    .attr("r", 3)
+                    .attr("r", function(d){return dotSize(d["WS"]);})
+                    .attr("fill", function(d){return dotColor(d["VORP"]);})
+                    
+    var xAxisGen = d3.axisBottom().scale(xScale)
+    var xAxis = svg_2.append("g")
+                    .call(xAxisGen)
+                    .style("transform", `translateY(${dimensions.height-dimensions.margin.bottom}px)`)
+                       
+    var yAxisGen = d3.axisLeft().scale(yScale)
+    var yAxis = svg_2.append("g")
+                   .call(yAxisGen)
+                   .style("transform", `translateX(${dimensions.margin.left}px)`)
+
+    svg_2.append("text")
+       .attr("class", "y label")
+       .attr("text-anchor", "end")
+       .text("VORP")
+       .attr("transform", "translate(40, 260) rotate(-90)")
+
+    svg_2.append("text")
+        .attr("class", "x label")
+        .attr("text-anchor", "end")
+        .text("Win Share")
+        .attr("transform", "translate(400, 565)")
 
   }
 )
