69c3c09a6d82ef049a905ce9587379f0f84d9b28
cblue13 <115600156+cblue13@users.noreply.github.com>
2022-11-16 22:17:19 -0500

    Update script_teamsuccess_viz2.js

--- script_teamsuccess_viz2.js
+++ script_teamsuccess_viz2.js
@@ -3,8 +3,8 @@ d3.csv("nba_adv_data.csv").then(
   function(dataset) {
 
     var dimensions = {
-      width: 500,
-      height: 450,
+      width: 700,
+      height: 600,
       margin:{
           top: 60,
           bottom: 70,
@@ -24,7 +24,7 @@ d3.csv("nba_adv_data.csv").then(
                     .append("rect")
                     .attr("stroke-width", 2)
                     .attr("stroke", "black")
-                    .attr("fill", "none")
+                    .attr("fill", "None")
                     .attr("y", 25)
                     .attr("width", dimensions.width)
                     .attr("height", dimensions.height)
@@ -42,12 +42,15 @@ d3.csv("nba_adv_data.csv").then(
     var yAccessor = d => d["VORP"]
 
     var xScale = d3.scaleLinear()
-                   .domain(d3.extent(dataset, xAccessor))
+                   .domain([-.6, 2.7]) 
                    .range([dimensions.margin.left, dimensions.width - dimensions.margin.right])
     
     var yScale = d3.scaleLinear()
-                   .domain(d3.extent(dataset, yAccessor))
+                   .domain([-.4, 1.6])
                    .range([dimensions.height-dimensions.margin.bottom, dimensions.margin.top])
+  
+    var dotColor = d3.scaleLinear().domain(yScale.domain()).range(["#8790b1", "#ff6818"])
+    var dotSize = d3.scaleLinear().domain(xScale.domain()).range([2, 6])
 
     var dots = svg_2.append("g")
                     .selectAll("circle")
@@ -56,19 +59,31 @@ d3.csv("nba_adv_data.csv").then(
                     .append("circle")
                     .attr("cx", d => xScale(xAccessor(d)))
                     .attr("cy", d => yScale(yAccessor(d)))
-                    .attr("r", 2.5)
-                    .attr("fill", "black")
+                    .attr("r", 3)
+                    .attr("r", function(d){return dotSize(d["WS"]);})
+                    .attr("fill", function(d){return dotColor(d["VORP"]);})
                     
     var xAxisGen = d3.axisBottom().scale(xScale)
     var xAxis = svg_2.append("g")
                     .call(xAxisGen)
                     .style("transform", `translateY(${dimensions.height-dimensions.margin.bottom}px)`)
-                    
-                    
+                       
     var yAxisGen = d3.axisLeft().scale(yScale)
     var yAxis = svg_2.append("g")
                    .call(yAxisGen)
                    .style("transform", `translateX(${dimensions.margin.left}px)`)
+
+    svg_2.append("text")
+       .attr("class", "y label")
+       .attr("text-anchor", "end")
+       .text("VORP")
+       .attr("transform", "translate(40, 260) rotate(-90)")
+
+    svg_2.append("text")
+        .attr("class", "x label")
+        .attr("text-anchor", "end")
+        .text("Win Share")
+        .attr("transform", "translate(400, 565)")
 
   }
 )
