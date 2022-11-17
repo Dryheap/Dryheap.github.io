89c47d3933cff4f98b91b9c26a79c22ec5c6dced
cblue13 <115600156+cblue13@users.noreply.github.com>
2022-11-16 19:44:22 -0500

    Update script_teamsuccess_viz2.js

--- script_teamsuccess_viz2.js
+++ script_teamsuccess_viz2.js
@@ -3,8 +3,8 @@ d3.csv("nba_adv_data.csv").then(
   function(dataset) {
 
     var dimensions = {
-      width: 1000,
-      height: 700,
+      width: 500,
+      height: 450,
       margin:{
           top: 60,
           bottom: 70,
@@ -13,21 +13,21 @@ d3.csv("nba_adv_data.csv").then(
       }
     }
     var svg_2 =  d3.select("#teamsuccess_viz2")
-      .style("width", dimensions.width*0.7)
+      .style("width", dimensions.width)
       .style("height", dimensions.height)
       .attr("x", 850)
       //.attr("transform", "translate(300,155)")
 
     console.log(dimensions.width)
 
-    let border_2 = svg_2.append("g")
+   let border_2 = svg_2.append("g")
                     .append("rect")
                     .attr("stroke-width", 2)
                     .attr("stroke", "black")
                     .attr("fill", "none")
                     .attr("y", 25)
-                    .attr("width", dimensions.width*0.6)
-                    .attr("height", dimensions.height-25)
+                    .attr("width", dimensions.width)
+                    .attr("height", dimensions.height)
 
     var title = svg_2.append("text") 
                     .text('Player Success') 
@@ -36,14 +36,39 @@ d3.csv("nba_adv_data.csv").then(
                     .attr("dy", 20)
                     .attr("dx", border_2.attr("width") / 2);
 
-    var temp_holder_2 = svg_2.append("text")
-                        .text('<Visualization 2>')
-                        .attr("text-anchor", "middle")
-                        .style("font-size", "12px")
-                        .attr("dx", border_2.attr("width")/2)
-                        .attr("dy", border_2.attr("height")/2)
+    var players = d3.map(dataset, function(d){return d["Player"]})
 
-    console.log(border_2.attr("width"))
+    var xAccessor = d => d["WS"]
+    var yAccessor = d => d["VORP"]
+
+    var xScale = d3.scaleLinear()
+                   .domain(d3.extent(dataset, xAccessor))
+                   .range([dimensions.margin.left, dimensions.width - dimensions.margin.right])
+    
+    var yScale = d3.scaleLinear()
+                   .domain(d3.extent(dataset, yAccessor))
+                   .range([dimensions.height-dimensions.margin.bottom, dimensions.margin.top])
+
+    var dots = svg_2.append("g")
+                    .selectAll("circle")
+                    .data(dataset, function(d){return d})
+                    .enter()
+                    .append("circle")
+                    .attr("cx", d => xScale(xAccessor(d)))
+                    .attr("cy", d => yScale(yAccessor(d)))
+                    .attr("r", 2.5)
+                    .attr("fill", "black")
+                    
+    var xAxisGen = d3.axisBottom().scale(xScale)
+    var xAxis = svg_2.append("g")
+                    .call(xAxisGen)
+                    .style("transform", `translateY(${dimensions.height-dimensions.margin.bottom}px)`)
+                    
+                    
+    var yAxisGen = d3.axisLeft().scale(yScale)
+    var yAxis = svg_2.append("g")
+                   .call(yAxisGen)
+                   .style("transform", `translateX(${dimensions.margin.left}px)`)
 
   }
 )
