2107cd1c907f2de116b9a77967aa59812a62cac8
cblue13 <115600156+cblue13@users.noreply.github.com>
2022-11-28 12:40:43 -0500

    Update script_spiderchart.js

--- script_spiderchart.js
+++ script_spiderchart.js
@@ -42,18 +42,13 @@ d3.csv("nba_adv_data.csv").then(
 
 
     var maxValue = 500;
-    var radius = 80;
+    var radius = 200;
     var center = {x:50, y: 50};
                     
     const radialScale = d3.scaleLinear()
                       .domain([0, maxValue]) 
                       .range([radius, 0]) 
-    var axis = d3.axisRight()
-                  .scale(radialScale)
-                  .ticks(5)
-    spiderchart_svg.append('g')
-       .attr('transform', `translate(${center.x},${center.y  - radius})`)
-       .call(axis);
+
 
     let val, angle;
     for (val = 0; val <= maxValue; val += maxValue / 5) {
@@ -62,9 +57,31 @@ d3.csv("nba_adv_data.csv").then(
            .attr('cx', center.x)
            .attr('cy', center.y)
            .attr('r', r)
+           .attr("transform", "translate(255, 300)")
            .style('stroke', '#aaa')
            .style('fill', 'none');
        }
 
+      function axisAngle(angle, val){
+        let x = Math.cos(angle) * radialScale(val)
+        let y = Math.sin(angle) * radialScale(val)
+        return {"x":300 + x, "y": 300 - y}
+      }
+
+      for (var i = 0; i < 5; i++){
+        let angle = (Math.PI / 2) + (2 * Math.PI * i / 5);
+        let line_coord = axisAngle(angle, 10);
+        let label_coord = axisAngle(angle, 10.5)
+      
+
+      spiderchart_svg.append("line")
+                    .attr("x1", 300)
+                    .attr("y1", 300)
+                    .attr("x2", line_coord.x)
+                    .attr("y2", line_coord.y)
+                    .attr("stroke", "black")
+                    .attr("transform", "translate(5, 45)");
+
+      }
   }
 )
