f5862ce68f8297d5567b720d9caa13b446cc31c3
Kaleb Knecht <kknecht@clemson.edu>
2022-12-01 13:56:04 -0500

    spider chart appear automatically

--- script_teamsuccess_viz2.js
+++ script_teamsuccess_viz2.js
@@ -1,3 +1,44 @@
+d3.csv("nba_adv_data.csv").then(
+
+  function(dataset) {
+
+    var dimensions = {
+      width: 700,
+      height: 600,
+      margin:{
+          top: 60,
+          bottom: 70,
+          right: 10,
+          left: 100
+      }
+    }
+    var svg_2 =  d3.select("#teamsuccess_viz2")
+      .style("width", dimensions.width)
+      .style("height", dimensions.height)
+      .attr("x", 950)
+      //.attr("transform", "translate(300,155)")
+
+
+    let teamviz2_border = svg_2.append("g")
+                  .append("rect")
+                  .attr("stroke-width", 2)
+                  .attr("stroke", "black")
+                  .attr("fill", "none")
+                  .attr("y", 25)
+                  .attr("width", dimensions.width*0.6)
+                  .attr("height", dimensions.height-25)
+
+
+  var title = svg_2.append("text")
+                  .attr("id", "og_team_radar_text")
+                  .text("Test") 
+                  .attr("text-anchor", "middle") 
+                  .style("font-size", '24px') 
+                  .attr("dy", 20)
+                  .attr("dx", teamviz2_border.attr("width") / 2);
+  }
+)
+
 
 function setTeam(team) {
   d3.csv("nba_adv_data.csv").then(
@@ -14,7 +55,11 @@ function setTeam(team) {
             left: 100
         }
       }
-      var svg_2 =  d3.select("#teamsuccess_viz2")
+
+      d3.select("#teamsuccess_viz2")
+        .remove()
+        
+      svg_2 =  d3.select("#teamsuccess_viz2")
         .style("width", dimensions.width)
         .style("height", dimensions.height)
         .attr("x", 950)
@@ -31,7 +76,7 @@ function setTeam(team) {
                     .attr("width", dimensions.width*0.6)
                     .attr("height", dimensions.height-25)
 
-
+    
     var title = svg_2.append("text") 
                     .text(team) 
                     .attr("text-anchor", "middle") 
