3d90ee71ca00336b197f0e4ee76003826bec452e
cblue13 <115600156+cblue13@users.noreply.github.com>
2022-12-01 20:12:33 -0500

    Update script_spiderchart.js

--- script_spiderchart.js
+++ script_spiderchart.js
@@ -12,7 +12,7 @@ d3.csv("nba_adv_data.csv").then(
           left: 80
       }
     }
-
+    var features = ["WS", "VORP", "TS%", "BPM", "FG%"]
     var spiderchart_svg =  d3.select("#spiderchart")
       .style("width", dimensions.width*0.6)
       .style("height", dimensions.height)
@@ -48,40 +48,82 @@ d3.csv("nba_adv_data.csv").then(
     const radialScale = d3.scaleLinear()
                       .domain([0, maxValue]) 
                       .range([radius, 0]) 
+    let ticks = [100, 200, 300, 400]
+    ticks.forEach(t=> 
+      spiderchart_svg.append("circle")
+                     .attr("cx", 300)
+                     .attr("cy", 300)
+                     .attr("fill", "none")
+                     .attr("stroke", "gray")
+                     .attr("r", radialScale(t))
+      )
 
+    ticks.forEach(t=>
+      spiderchart_svg.append("text")
+                     .attr("x", 305)
+                     .attr("y", 300 - radialScale(t))
+                     .text(t.toString())
+      )
 
-    let val, angle;
-    for (val = 0; val <= maxValue; val += maxValue / 5) {
-      const r = radialScale(val);
-         spiderchart_svg.append('circle')
-           .attr('cx', center.x)
-           .attr('cy', center.y)
-           .attr('r', r)
-           .attr("transform", "translate(255, 300)")
-           .style('stroke', '#aaa')
-           .style('fill', 'none');
-       }
+    function angleToCoordinate(angle, value){
+        let x = Math.cos(angle) * radialScale(value);
+        let y = Math.sin(angle) * radialScale(value);
+        return {"x": 300 + x, "y": 300 - y};
+    }
 
-      function axisAngle(angle, val){
-        let x = Math.cos(angle) * radialScale(val)
-        let y = Math.sin(angle) * radialScale(val)
-        return {"x":300 + x, "y": 300 - y}
-      }
-
-      for (var i = 0; i < 5; i++){
-        let angle = (Math.PI / 2) + (2 * Math.PI * i / 5);
-        let line_coord = axisAngle(angle, 10);
-        let label_coord = axisAngle(angle, 10.5)
-      
-
+    for (var i = 0; i < features.length; i++) {
+      let ft_name = features[i];
+      let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+      let line_coordinate = angleToCoordinate(angle, 10);
+      let label_coordinate = angleToCoordinate(angle, 10.5);
+      console.log("h")
+      //draw axis line
       spiderchart_svg.append("line")
-                    .attr("x1", 300)
-                    .attr("y1", 300)
-                    .attr("x2", line_coord.x)
-                    .attr("y2", line_coord.y)
-                    .attr("stroke", "black")
-                    .attr("transform", "translate(5, 45)");
+      .attr("x1", 300)
+      .attr("y1", 300)
+      .attr("x2", line_coordinate.x)
+      .attr("y2", line_coordinate.y)
+      .attr("stroke","black");
+  
+      //draw axis label
+      spiderchart_svg.append("text")
+      .attr("x", label_coordinate.x)
+      .attr("y", label_coordinate.y)
+      .text(ft_name);
+    }
 
+    let line = d3.line()
+    .x(d => d.x)
+    .y(d => d.y);
+
+    let colors = ["darkorange", "gray", "navy"];
+
+    function getPathCoordinates(data_point){
+      let coordinates = [];
+      for (var i = 0; i < features.length; i++){
+          let ft_name = features[i];
+          let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+          coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
       }
+      return coordinates;
+  }
+
+    for (var i = 0; i < dataset.length; i++){
+      
+      let d = dataset["WS"];
+      let color = colors["WS"];
+      let coordinates = getPathCoordinates(function(d){return d["WS"]})
+  
+      //draw the path element
+      spiderchart_svg.append("path")
+      .datum(coordinates)
+      .attr("d",line)
+      .attr("stroke-width", 3)
+      .attr("stroke", color)
+      .attr("fill", color)
+      .attr("stroke-opacity", 1)
+      .attr("opacity", 0.5);
+  }
+   
   }
 )
